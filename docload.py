8e3eb1701748e816f0160d9dcd275b5fbc55ac54
Antelope10 <yejun.yun2006@gmail.com>
2023-05-28 11:24:32 -0500

    Initial commit

---
+++ docload.py
@@ -0,0 +1,98 @@
+"""
+Loads a google doc into a file called doc.txt, one paragraph per line.
+Replace the DOCUMENT_ID variable with your own.
+"""
+
+from __future__ import print_function
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+
+# The ID of a sample document.
+DOCUMENT_ID = '14J_TlsjHUXdw65tm2sN_DuKYlzf97zC9qUQ1nrqEpg8'
+
+def read_paragraph_element(element):
+    """Returns the text in the given ParagraphElement.
+
+        Args:
+            element: a ParagraphElement from a Google Doc.
+    """
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
+    
+def read_structural_elements(elements):
+    """Recurses through a list of Structural Elements to read a document's text where text may be
+        in nested elements.
+
+        Args:
+            elements: a list of Structural Elements.
+    """
+    text = ''
+    for value in elements:
+        if 'paragraph' in value:
+            elements = value.get('paragraph').get('elements')
+            for elem in elements:
+                text += read_paragraph_element(elem)
+        elif 'table' in value:
+            # The text in table cells are in nested Structural Elements and tables may be
+            # nested.
+            table = value.get('table')
+            for row in table.get('tableRows'):
+                cells = row.get('tableCells')
+                for cell in cells:
+                    text += read_structural_elements(cell.get('content'))
+        elif 'tableOfContents' in value:
+            # The text in the TOC is also in a Structural Element.
+            toc = value.get('tableOfContents')
+            text += read_structural_elements(toc.get('content'))
+    return text
+
+
+def main():
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('docs', 'v1', credentials=creds)
+
+        # Retrieve the documents contents from the Docs service.
+        document = service.documents().get(documentId=DOCUMENT_ID).execute()
+        doc_content = read_structural_elements(document.get('body').get('content'))
+        doc_content.split('\n')
+        with open('doc.txt', 'w') as f:
+            for element in doc_content:
+                f.write(element)
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
---
+++ prepare.py
@@ -0,0 +1,54 @@
+"""
+Takes a specified text file and summarizes each line using GPT.
+Appends the summary-original pairs as json objects to a specified .jsonl file. 
+Modifies data slightly so it can be used for fine-tuning via the OpenAI API.
+"""
+
+import os
+import openai
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model = "gpt-3.5-turbo"
+jsonl_file = "data/5-28.jsonl"
+doc_file = "doc.txt"
+
+#appends prompt+completion string pair as a json object to file.jsonl
+def append_to_jsonl(prompt, completion, file):
+    data = {
+        'prompt': prompt,
+        'completion': completion
+    }
+    
+    json_data = json.dumps(data)
+    
+    if os.path.exists(file):
+        append_write = 'a'  # Append to the existing file.
+    else:
+        append_write = 'w'  # Create the file if it doesn't exist.
+
+    with open(file, append_write) as file:
+        file.write(json_data)
+        file.write('\n')
+
+
+#reads doc.txt, creates summary of every line, and appends summary-original text pair to data.jsonl
+with open(doc_file, 'r') as f:
+    lines = f.readlines()
+for element in lines:
+    completion = openai.ChatCompletion.create(
+        model=model,
+        temperature=0,
+        max_tokens=256,
+        messages=[
+            {"role": "system", "content": "You are given an excerpt from a novel. Compress the excerpt as tightly as possible while keeping any important details, such as characters, references to dialogues, and passage of time."},
+            {"role": "user", "content": element}
+        ]
+    )
+    append_to_jsonl(completion.choices[0].message.content + "\n\n###\n\n"," " + element + "###", jsonl_file)
+
+
+        
