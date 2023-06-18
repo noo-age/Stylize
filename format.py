9b3bc661ed052331843e0f5bf89bc0f37d564d51
Antelope10 <yejun.yun2006@gmail.com>
2023-06-18 18:03:11 -0500

    Changed to a script that uses GPT to clean text, one paragraph per line

--- format.py
+++ format.py
@@ -1,3 +1,36 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model = "gpt-3.5-turbo-16k"
+input = "texts/devil.txt"
+output = "texts/" + input[5:-5] + "_cleaned.txt"
+system_message = "You will be given an excerpt of text from a novel. Remove all headers, quotes, and unnecessary newlines. Reformat the text so that it is correctly broken up into paragraphs if necessary. Make sure to separate dialogue into paragraphs as well."
+
+def chunk_string(s, chunk_size=6000):
+    return [s[i:i+chunk_size] for i in range(0, len(s), chunk_size)]
+
+half_cleaned_text = ""
+
+with open(input, 'r') as f:
+    text = f.read()
+    text = chunk_string(text)
+    for element in text:
+        completion = openai.ChatCompletion.create(
+        model=model,
+        temperature=0,
+        max_tokens=2048,
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": element}
+        ]
+        )
+        half_cleaned_text += completion.choices[0].message.content
+        
+    
 
 with open("texts/crime.txt", "r") as input_file, open("output_novel.txt", "w") as output_file:
     paragraph = []
