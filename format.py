d26829164aa4985ef8f448ba836f5d7dc7c5f862
Antelope10 <yejun.yun2006@gmail.com>
2023-06-18 18:43:29 -0500

    Mostly finished, although it still doesn't remove headers

--- format.py
+++ format.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 model = "gpt-3.5-turbo-16k"
-input = "texts/devil.txt"
-output = "texts/" + input[5:-5] + "_cleaned.txt"
+input = "texts/test.txt"
+output = "texts/" + input[5:-4] + "_cleaned.txt"
 system_message = "You will be given an excerpt of text from a novel. Remove all headers, quotes, and unnecessary newlines. Reformat the text so that it is correctly broken up into paragraphs if necessary. Make sure to separate dialogue into paragraphs as well."
 
 def chunk_string(s, chunk_size=6000):
@@ -29,17 +29,8 @@ with open(input, 'r') as f:
         ]
         )
         half_cleaned_text += completion.choices[0].message.content
-        
-    
 
-with open("texts/crime.txt", "r") as input_file, open("output_novel.txt", "w") as output_file:
-    paragraph = []
-    for line in input_file:
-        line = line.strip()
-        if line:
-            paragraph.append(line)
-        else:
-            output_file.write(" ".join(paragraph) + "\n")
-            paragraph = []
-    if paragraph:
-        output_file.write(" ".join(paragraph) + "\n")
+half_cleaned_text = half_cleaned_text.replace("\n\n", "\n")
+
+with open(output, "w") as f:
+    f.write(half_cleaned_text)
