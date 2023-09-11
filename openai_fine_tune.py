d88ae519379c333977debeb42919db29480bb548
Antelope10 <yejun.yun2006@gmail.com>
2023-09-11 08:15:25 -0500

    Slight modification of openai's fine-tuning example in docs

--- openai_fine_tune.py
+++ openai_fine_tune.py
@@ -2,27 +2,28 @@ import openai
 import os
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.File.create(
-  file=open("mydata.jsonl", "rb"),
+file = openai.File.create(
+  file=open("data/09-06_chat.jsonl", "rb"),
   purpose='fine-tune'
 )
 
-openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+
+tune_job = openai.FineTuningJob.create(training_file='file-s85K5b9KeE6uTmfNPt4MfGDY', model="gpt-3.5-turbo")
 
 # List 10 fine-tuning jobs
 openai.FineTuningJob.list(limit=10)
 
 # Retrieve the state of a fine-tune
-openai.FineTuningJob.retrieve("ft-abc123")
+openai.FineTuningJob.retrieve(tune_job['id'])
 
 # Cancel a job
-openai.FineTuningJob.cancel("ft-abc123")
+#openai.FineTuningJob.cancel(tune_job['id'])
 
 # List up to 10 events from a fine-tuning job
-openai.FineTuningJob.list_events(id="ft-abc123", limit=10)
+openai.FineTuningJob.list_events(id=tune_job['id'], limit=10)
 
 # Delete a fine-tuned model (must be an owner of the org the model was created in)
-openai.Model.delete("ft-abc123")
+#openai.Model.delete("ft-abc123")
 
 completion = openai.ChatCompletion.create(
   model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
